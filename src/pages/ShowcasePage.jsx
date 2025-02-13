9c3e2da78d26a33995d25ac70782c1fe7853843b
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-02-13 19:04:43 +1100

    updated bg video

--- src/pages/ShowcasePage.jsx
+++ src/pages/ShowcasePage.jsx
@@ -4,10 +4,11 @@ const ShowcasePage = () => {
   return (
     <>
     <div className="h-screen text-white flex justify-center items-center text-heading-1 font-black">
-      <video src={video} autoPlay muted loop className="absolute inset-0 object-cover w-full h-full z-0 pointer-events-none" />
+      <video src={video} controls={false} autoPlay muted loop className="absolute inset-0 object-cover w-full h-full z-0 pointer-events-none" />
       <div className="absolute inset-0 bg-black/50 z-[1]"></div>
       <h1 className="relative z-[2] text-white">
-        This is the Showcase Page
+        HI!
+        
       </h1>
       </div>
       <section className="h-screen bg-black">
