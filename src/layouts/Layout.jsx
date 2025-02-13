a51c9107373897b7d5dbbbfdb9b07808d9c60af3
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-02-13 17:29:46 +1100

    Revert "Update Layout.jsx"
    This reverts commit 1d57bead745c2d67ab004f047c3836e5f6d8ff79.

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 import { motion, useIsPresent } from "framer-motion";
 import CamCorners from "../components/CamCorners";
 
