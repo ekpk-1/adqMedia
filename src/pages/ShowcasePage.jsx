00904acb7b456715f4a8af1be7efbd80c74f2540
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-02-13 19:06:44 +1100

    Update ShowcasePage.jsx

--- src/pages/ShowcasePage.jsx
+++ src/pages/ShowcasePage.jsx
@@ -4,11 +4,23 @@ const ShowcasePage = () => {
   return (
     <>
     <div className="h-screen text-white flex justify-center items-center text-heading-1 font-black">
-      <video src={video} controls={false} autoPlay muted loop className="absolute inset-0 object-cover w-full h-full z-0 pointer-events-none" />
+      <video 
+        src={video} 
+        autoPlay 
+        muted 
+        loop 
+        playsInline
+        disablePictureInPicture
+        className="absolute inset-0 object-cover w-full h-full z-0 pointer-events-none" 
+        style={{ 
+          WebkitUserSelect: 'none',
+          msUserSelect: 'none',
+          userSelect: 'none',
+        }}
+      />
       <div className="absolute inset-0 bg-black/50 z-[1]"></div>
       <h1 className="relative z-[2] text-white">
         HI!
-        
       </h1>
       </div>
       <section className="h-screen bg-black">
