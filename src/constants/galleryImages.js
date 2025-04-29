b93b1e5ad8f2a881b2f36c88dc81fa779b9b1dac
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-04-29 18:32:13 +1000

    updated gallery section

--- src/constants/galleryImages.js
+++ src/constants/galleryImages.js
@@ -1,213 +1,246 @@
 export const galleryImages = [
-  //N8Evo row
+  //tookieEVO
+  {
+    id: 0,
+    src: '/gallery/tookie3.jpg',
+    alt: 'Gallery Image 0',
+    span: 'col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]'  
+  },
   {
     id: 1,
-    src: '/gallery/n8dawg3.jpg',
+    src: '/gallery/tookie1.jpg',
     alt: 'Gallery Image 1',
-    span: 'col-span-6 row-start-1 aspect-square row-span-2 max-sm:col-span-12 max-sm:row-span-1'  
+    span: 'col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]'  
   },
   {
     id: 2,
-    src: '/gallery/n8dawg43.jpg',
+    src: '/gallery/tookie2.jpg',
     alt: 'Gallery Image 2',
-    span: 'col-span-3 row-start-1 aspect-square max-sm:col-span-6 max-sm:row-start-2' 
-  },
-  {
-    id: 3,
-    src: '/gallery/n8dawg45.jpg',
-    alt: 'Gallery Image 3',
-    span: 'col-span-3 row-start-2 aspect-square max-sm:col-span-6 max-sm:row-start-2'  
+    span: 'col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]'  
   },
   {
     id: 4,
-    src: '/gallery/evoN83.jpg',
+    src: '/gallery/tookie5.jpg',
     alt: 'Gallery Image 4',
-    span: 'col-span-3 row-start-1 aspect-square max-sm:col-span-6 max-sm:row-start-3'  
+    span: 'col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-12 max-sm:aspect-[2/1]'  
+  },
+  {
+    id: 3,
+    src: '/gallery/tookie4.jpg',
+    alt: 'Gallery Image 3',
+    span: 'col-span-4 lg:col-span-8 aspect-[2/1] max-sm:col-span-6 max-sm:aspect-[3/3]'  
   },
   {
     id: 5,
-    src: '/gallery/n8dawg42.jpg',
+    src: '/gallery/tookie6.jpg',
     alt: 'Gallery Image 5',
-    span: 'col-span-3 row-start-2 aspect-square max-sm:col-span-6 max-sm:row-start-3'  
+    span: 'col-span-4 lg:col-span-4 aspect-[4/4.2] max-sm:col-span-6 max-sm:aspect-[3/3]'  
   },
-  //YellowEvo row
+
+  //N8Evo row
   {
     id: 6,
-    src: '/gallery/evo1.jpg',
+    src: '/gallery/n8dawg3.jpg',
     alt: 'Gallery Image 6',
-    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 '  
+    span: 'col-span-6 aspect-square row-span-2 max-sm:col-span-12 max-sm:row-span-1'  
   },
   {
     id: 7,
-    src: '/gallery/evo4.jpg',
+    src: '/gallery/n8dawg43.jpg',
     alt: 'Gallery Image 7',
-    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-12 max-sm:row-start-4'  
+    span: 'col-span-3 aspect-square max-sm:col-span-6' 
   },
   {
     id: 8,
-    src: '/gallery/evo5.jpg',
+    src: '/gallery/n8dawg45.jpg',
     alt: 'Gallery Image 8',
-    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 max-sm:row-start-5'  
+    span: 'col-span-3 aspect-square max-sm:col-span-6'  
   },
-  //EK
   {
     id: 9,
-    src: '/gallery/civicEK.jpg',
+    src: '/gallery/evoN83.jpg',
     alt: 'Gallery Image 9',
-    span: 'col-span-4 lg:col-span-12 aspect-[16/10] max-sm:col-span-12'  
+    span: 'col-span-3 aspect-square max-sm:col-span-6'  
   },
   {
     id: 10,
-    src: '/gallery/ek5.jpg',
+    src: '/gallery/n8dawg42.jpg',
     alt: 'Gallery Image 10',
-    span: 'col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12'  
+    span: 'col-span-3 aspect-square max-sm:col-span-6'  
   },
+  //YellowEvo row
   {
     id: 11,
-    src: '/gallery/ek4.jpg',
+    src: '/gallery/evo1.jpg',
     alt: 'Gallery Image 11',
-    span: 'col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12'  
+    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 '  
   },
   {
     id: 12,
-    src: '/gallery/ekBay2.jpg',
+    src: '/gallery/evo4.jpg',
     alt: 'Gallery Image 12',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6 max-sm:row-start-9'  
+    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-12'  
   },
   {
     id: 13,
-    src: '/gallery/ekBay1.jpg',
+    src: '/gallery/evo5.jpg',
     alt: 'Gallery Image 13',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6 max-sm:row-start-9'  
+    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 max-sm:row-start-8'  
   },
+  //EK
   {
     id: 14,
-    src: '/gallery/ek2.jpg',
+    src: '/gallery/civicEK.jpg',
     alt: 'Gallery Image 14',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6 max-sm:row-start-8'  
+    span: 'col-span-4 lg:col-span-12 aspect-[16/10] max-sm:col-span-12'  
   },
   {
     id: 15,
-    src: '/gallery/ek3.jpg',
+    src: '/gallery/ek5.jpg',
     alt: 'Gallery Image 15',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6 max-sm:row-start-8'  
+    span: 'col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12'  
   },
-  //chaser
   {
     id: 16,
-    src: '/gallery/chaser3.jpg',
+    src: '/gallery/ek4.jpg',
     alt: 'Gallery Image 16',
-    span: 'col-span-4 lg:col-span-8 lg:row-span-2 aspect-[4/3] max-sm:col-span-12 '  
+    span: 'col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12'  
   },
   {
     id: 17,
-    src: '/gallery/chaser5.jpg',
+    src: '/gallery/ekBay2.jpg',
     alt: 'Gallery Image 17',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
   },
   {
     id: 18,
-    src: '/gallery/chaser4.jpg',
+    src: '/gallery/ekBay1.jpg',
     alt: 'Gallery Image 18',
+    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
+  },
+  {
+    id: 19,
+    src: '/gallery/ek2.jpg',
+    alt: 'Gallery Image 19',
+    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
+  },
+  {
+    id: 20,
+    src: '/gallery/ek3.jpg',
+    alt: 'Gallery Image 20',
+    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
+  },
+  //chaser
+  {
+    id: 21,
+    src: '/gallery/chaser3.jpg',
+    alt: 'Gallery Image 21',
+    span: 'col-span-4 lg:col-span-8 lg:row-span-2 aspect-[4/3] max-sm:col-span-12 '  
+  },
+  {
+    id: 22,
+    src: '/gallery/chaser5.jpg',
+    alt: 'Gallery Image 22',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+  },
+  {
+    id: 23,
+    src: '/gallery/chaser4.jpg',
+    alt: 'Gallery Image 23',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
   },
   //others
   {
-    id: 19,
+    id: 24,
     src: '/gallery/s2k.jpg',
-    alt: 'Gallery Image 19',
+    alt: 'Gallery Image 24',
     span: 'col-span-6 aspect-[4/3] max-sm:col-span-12'  
   },
   {
-    id: 20,
+    id: 25,
     src: '/gallery/r35s.jpg',
-    alt: 'Gallery Image 20',
+    alt: 'Gallery Image 25',
     span: 'col-span-6 aspect-[4/3] max-sm:col-span-12'  
   },
   //86
   {
-    id: 21,
+    id: 26,
     src: '/gallery/861.jpg',
-    alt: 'Gallery Image 21',
+    alt: 'Gallery Image 26',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12'  
   },
   {
-    id: 22,
+    id: 27,
     src: '/gallery/862.jpg',
-    alt: 'Gallery Image 22',
+    alt: 'Gallery Image 27',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
   },  
   {
-    id: 23,
-    src: '/gallery/863.jpg',
-    alt: 'Gallery Image 23',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 24,
-    src: '/gallery/864.jpg',
-    alt: 'Gallery Image 24',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 25,
-    src: '/gallery/865.jpg',
-    alt: 'Gallery Image 25',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  //jazz
-  {
-    id: 26,
-    src: '/gallery/jazz1.jpg',
-    alt: 'Gallery Image 26',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 27,
-    src: '/gallery/jazz2.jpg',
-    alt: 'Gallery Image 27',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
     id: 28,
-    src: '/gallery/jazz3.jpg',
+    src: '/gallery/863.jpg',
     alt: 'Gallery Image 28',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
   },
   {
     id: 29,
-    src: '/gallery/jazz4.jpg',
+    src: '/gallery/864.jpg',
     alt: 'Gallery Image 29',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+  },
+  {
+    id: 30,
+    src: '/gallery/865.jpg',
+    alt: 'Gallery Image 30',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+  },
+  //jazz
+  {
+    id: 31,
+    src: '/gallery/jazz1.jpg',
+    alt: 'Gallery Image 31',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+  },
+  {
+    id: 32,
+    src: '/gallery/jazz2.jpg',
+    alt: 'Gallery Image 32',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+  },
+  {
+    id: 33,
+    src: '/gallery/jazz3.jpg',
+    alt: 'Gallery Image 33',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+  },
+  {
+    id: 34,
+    src: '/gallery/jazz4.jpg',
+    alt: 'Gallery Image 34',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
   },
   //rari
   {
-    id: 30,
+    id: 35,
     src: '/gallery/rari.jpg',
-    alt: 'Gallery Image 30',
+    alt: 'Gallery Image 35',
     span: 'col-span-4 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]'  
   },
   {
-    id: 31,
+    id: 36,
     src: '/gallery/rari2.jpg',
-    alt: 'Gallery Image 31',
+    alt: 'Gallery Image 36',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
   }, 
   {
-    id: 32,
+    id: 37,
     src: '/gallery/rari3.jpg',
-    alt: 'Gallery Image 32',
+    alt: 'Gallery Image 37',
     span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
   }, 
   {
-    id: 33,
+    id: 38,
     src: '/gallery/rari4.jpg',
-    alt: 'Gallery Image 33',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
+    alt: 'Gallery Image 38',
+    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12 '  
   } 
-  
-
-
-
-  
 ]; 
