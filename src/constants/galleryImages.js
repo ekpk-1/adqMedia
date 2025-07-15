227292345205481ae50c69ee13bb09ff4d3a86ba
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-07-15 14:52:00 +1000

    Removed some photos

--- src/constants/galleryImages.js
+++ src/constants/galleryImages.js
@@ -1,326 +1,307 @@
 export const galleryImages = [
-  //Yellow996
-  {
-    id: 46,
-    src: '/gallery/996-1.webp',
-    alt: '',
-    span: 'col-span-12'
-  },
-  {
-    id: 47,
-    src: '/gallery/996-2.webp',
-    alt: '',
-    span: 'col-span-4'
-  },
-  {
-    id: 48,
-    src: '/gallery/996-3.webp',
-    alt: '',
-    span: 'col-span-4'
-  },
-  {
-    id: 49,
-    src: '/gallery/996-4.webp',
-    alt: '',
-    span: 'col-span-4'
-  },
-  {
-    id: 50,
-    src: '/gallery/996-5.webp',
-    alt: '',
-    span: 'col-span-6'
-  },
-  {
-    id: 51,
-    src: '/gallery/996-6.webp',
-    alt: '',
-    span: 'col-span-6'
-  },
-  {
-    id: 52,
-    src: '/gallery/996-7.webp',
-    alt: '',
-    span: 'col-span-12'
-  },
-  //rushsM3
-  {
-    id: 1,
-    src: '/gallery/rushsM3-1.jpg',
-    alt: 'Rushs M3 - Front View',
-    span: 'col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 2,
-    src: '/gallery/rushsM3-5.jpg',
-    alt: 'Rushs M3 - Side View',
-    span: 'col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 3,
-    src: '/gallery/rushsM3-3.jpg',
-    alt: 'Rushs M3 - Interior',
-    span: 'col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 4,
-    src: '/gallery/rushsM3-4.jpg',
-    alt: 'Rushs M3 - Detail Shot',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 5,
-    src: '/gallery/rushsM3-6.jpg',
-    alt: 'Rushs M3 - Rear View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 6,
-    src: '/gallery/rushsM3-2.jpg',
-    alt: 'Rushs M3 - Profile View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12'  
-  },
-  //tookieEVO
-  {
-    id: 7,
-    src: '/gallery/tookie3.jpg',
-    alt: 'Tookie EVO - Front View',
-    span: 'col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 8,
-    src: '/gallery/tookie1.jpg',
-    alt: 'Tookie EVO - Side View',
-    span: 'col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 9,
-    src: '/gallery/tookie2.jpg',
-    alt: 'Tookie EVO - Interior',
-    span: 'col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 10,
-    src: '/gallery/tookie5.jpg',
-    alt: 'Tookie EVO - Detail Shot',
-    span: 'col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-12 max-sm:aspect-[2/1]'  
-  },
-  {
-    id: 11,
-    src: '/gallery/tookie4.jpg',
-    alt: 'Tookie EVO - Engine Bay',
-    span: 'col-span-4 lg:col-span-8 aspect-[2/1] max-sm:col-span-6 max-sm:aspect-[3/3] max-md:col-span-6'  
-  },
-  {
-    id: 12,
-    src: '/gallery/tookie6.jpg',
-    alt: 'Tookie EVO - Rear View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/4.2] max-sm:col-span-6 max-sm:aspect-[3/3] max-md:aspect-[2/1] max-md:col-span-6'  
-  },
+    //Yellow996
+    {
+        id: 46,
+        src: "/gallery/996-1.webp",
+        alt: "",
+        span: "col-span-12",
+    },
+    {
+        id: 47,
+        src: "/gallery/996-2.webp",
+        alt: "",
+        span: "col-span-4",
+    },
+    {
+        id: 48,
+        src: "/gallery/996-3.webp",
+        alt: "",
+        span: "col-span-4",
+    },
+    {
+        id: 49,
+        src: "/gallery/996-4.webp",
+        alt: "",
+        span: "col-span-4",
+    },
+    {
+        id: 50,
+        src: "/gallery/996-5.webp",
+        alt: "",
+        span: "col-span-6",
+    },
+    {
+        id: 51,
+        src: "/gallery/996-6.webp",
+        alt: "",
+        span: "col-span-6",
+    },
+    {
+        id: 52,
+        src: "/gallery/996-7.webp",
+        alt: "",
+        span: "col-span-12",
+    },
+    //rushsM3
+    {
+        id: 1,
+        src: "/gallery/rushsM3-1.jpg",
+        alt: "Rushs M3 - Front View",
+        span: "col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]",
+    },
+    {
+        id: 2,
+        src: "/gallery/rushsM3-5.jpg",
+        alt: "Rushs M3 - Side View",
+        span: "col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]",
+    },
+    {
+        id: 3,
+        src: "/gallery/rushsM3-3.jpg",
+        alt: "Rushs M3 - Interior",
+        span: "col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]",
+    },
+    {
+        id: 4,
+        src: "/gallery/rushsM3-4.jpg",
+        alt: "Rushs M3 - Detail Shot",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 5,
+        src: "/gallery/rushsM3-6.jpg",
+        alt: "Rushs M3 - Rear View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 6,
+        src: "/gallery/rushsM3-2.jpg",
+        alt: "Rushs M3 - Profile View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12",
+    },
+    //tookieEVO
+    {
+        id: 7,
+        src: "/gallery/tookie3.jpg",
+        alt: "Tookie EVO - Front View",
+        span: "col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]",
+    },
+    {
+        id: 8,
+        src: "/gallery/tookie1.jpg",
+        alt: "Tookie EVO - Side View",
+        span: "col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]",
+    },
+    {
+        id: 9,
+        src: "/gallery/tookie2.jpg",
+        alt: "Tookie EVO - Interior",
+        span: "col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]",
+    },
+    {
+        id: 10,
+        src: "/gallery/tookie5.jpg",
+        alt: "Tookie EVO - Detail Shot",
+        span: "col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-12 max-sm:aspect-[2/1]",
+    },
+    {
+        id: 11,
+        src: "/gallery/tookie4.jpg",
+        alt: "Tookie EVO - Engine Bay",
+        span: "col-span-4 lg:col-span-8 aspect-[2/1] max-sm:col-span-6 max-sm:aspect-[3/3] max-md:col-span-6",
+    },
+    {
+        id: 12,
+        src: "/gallery/tookie6.jpg",
+        alt: "Tookie EVO - Rear View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/4.2] max-sm:col-span-6 max-sm:aspect-[3/3] max-md:aspect-[2/1] max-md:col-span-6",
+    },
 
-  //N8Evo row
-  {
-    id: 13,
-    src: '/gallery/n8dawg3.jpg',
-    alt: 'N8 EVO - Front View',
-    span: 'col-span-6 aspect-square row-span-2 max-sm:col-span-12 max-sm:row-span-1'  
-  },
-  {
-    id: 14,
-    src: '/gallery/n8dawg43.jpg',
-    alt: 'N8 EVO - Side View',
-    span: 'col-span-3 aspect-square max-sm:col-span-6' 
-  },
-  {
-    id: 15,
-    src: '/gallery/n8dawg45.jpg',
-    alt: 'N8 EVO - Interior',
-    span: 'col-span-3 aspect-square max-sm:col-span-6'  
-  },
-  {
-    id: 16,
-    src: '/gallery/evoN83.jpg',
-    alt: 'N8 EVO - Detail Shot',
-    span: 'col-span-3 aspect-square max-sm:col-span-6'  
-  },
-  {
-    id: 17,
-    src: '/gallery/n8dawg42.jpg',
-    alt: 'N8 EVO - Engine Bay',
-    span: 'col-span-3 aspect-square max-sm:col-span-6'  
-  },
-  //YellowEvo row
-  {
-    id: 18,
-    src: '/gallery/evo1.jpg',
-    alt: 'Yellow EVO - Front View',
-    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 '  
-  },
-  {
-    id: 19,
-    src: '/gallery/evo4.jpg',
-    alt: 'Yellow EVO - Side View',
-    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-12'  
-  },
-  {
-    id: 20,
-    src: '/gallery/evo5.jpg',
-    alt: 'Yellow EVO - Interior',
-    span: 'col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 max-sm:row-start-16'  
-  },
-  //EK
-  {
-    id: 21,
-    src: '/gallery/civicEK.jpg',
-    alt: 'Civic EK - Front View',
-    span: 'col-span-4 lg:col-span-12 aspect-[16/10] max-sm:col-span-12 max-md:col-span-12'  
-  },
-  {
-    id: 22,
-    src: '/gallery/ek5.jpg',
-    alt: 'Civic EK - Side View',
-    span: 'col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6'  
-  },
-  {
-    id: 23,
-    src: '/gallery/ek4.jpg',
-    alt: 'Civic EK - Interior',
-    span: 'col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6'  
-  },
-  {
-    id: 24,
-    src: '/gallery/ekBay2.jpg',
-    alt: 'Civic EK - Engine Bay 1',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
-  },
-  {
-    id: 25,
-    src: '/gallery/ekBay1.jpg',
-    alt: 'Civic EK - Engine Bay 2',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
-  },
-  {
-    id: 26,
-    src: '/gallery/ek2.jpg',
-    alt: 'Civic EK - Detail Shot 1',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
-  },
-  {
-    id: 27,
-    src: '/gallery/ek3.jpg',
-    alt: 'Civic EK - Detail Shot 2',
-    span: 'col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6'  
-  },
-  //chaser
-  {
-    id: 28,
-    src: '/gallery/chaser3.jpg',
-    alt: 'Chaser - Front View',
-    span: 'col-span-4 lg:col-span-8 lg:row-span-2 aspect-[4/3] max-sm:col-span-12 max-md:col-span-8'  
-  },
-  {
-    id: 29,
-    src: '/gallery/chaser5.jpg',
-    alt: 'Chaser - Side View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6 max-md:col-span-6'  
-  },
-  {
-    id: 30,
-    src: '/gallery/chaser4.jpg',
-    alt: 'Chaser - Interior',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6 max-md:col-span-6'  
-  },
-  //others
-  {
-    id: 31,
-    src: '/gallery/s2k.jpg',
-    alt: 'S2000 - Front View',
-    span: 'col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6'  
-  },
-  {
-    id: 32,
-    src: '/gallery/r35s.jpg',
-    alt: 'R35 GTR - Front View',
-    span: 'col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6'  
-  },
-  //86
-  {
-    id: 33,
-    src: '/gallery/861.jpg',
-    alt: '86 - Front View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12'  
-  },
-  {
-    id: 34,
-    src: '/gallery/862.jpg',
-    alt: '86 - Side View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },  
-  {
-    id: 35,
-    src: '/gallery/863.jpg',
-    alt: '86 - Interior',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 36,
-    src: '/gallery/864.jpg',
-    alt: '86 - Detail Shot 1',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 37,
-    src: '/gallery/865.jpg',
-    alt: '86 - Detail Shot 2',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  //jazz
-  {
-    id: 38,
-    src: '/gallery/jazz1.jpg',
-    alt: 'Jazz - Front View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 39,
-    src: '/gallery/jazz2.jpg',
-    alt: 'Jazz - Side View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 40,
-    src: '/gallery/jazz3.jpg',
-    alt: 'Jazz - Interior',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  {
-    id: 41,
-    src: '/gallery/jazz4.jpg',
-    alt: 'Jazz - Detail Shot',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  },
-  //rari
-  {
-    id: 42,
-    src: '/gallery/rari.jpg',
-    alt: 'Ferrari - Front View',
-    span: 'col-span-4 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]'  
-  },
-  {
-    id: 43,
-    src: '/gallery/rari2.jpg',
-    alt: 'Ferrari - Side View',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  }, 
-  {
-    id: 44,
-    src: '/gallery/rari3.jpg',
-    alt: 'Ferrari - Interior',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6'  
-  }, 
-  {
-    id: 45,
-    src: '/gallery/rari4.jpg',
-    alt: 'Ferrari - Detail Shot',
-    span: 'col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12 '  
-  } 
-]; 
+    //N8Evo row
+    {
+        id: 13,
+        src: "/gallery/n8dawg3.jpg",
+        alt: "N8 EVO - Front View",
+        span: "col-span-6 aspect-square row-span-2 max-sm:col-span-12 max-sm:row-span-1",
+    },
+    {
+        id: 14,
+        src: "/gallery/n8dawg43.jpg",
+        alt: "N8 EVO - Side View",
+        span: "col-span-3 aspect-square max-sm:col-span-6",
+    },
+    {
+        id: 15,
+        src: "/gallery/n8dawg45.jpg",
+        alt: "N8 EVO - Interior",
+        span: "col-span-3 aspect-square max-sm:col-span-6",
+    },
+    {
+        id: 16,
+        src: "/gallery/evoN83.jpg",
+        alt: "N8 EVO - Detail Shot",
+        span: "col-span-3 aspect-square max-sm:col-span-6",
+    },
+    {
+        id: 17,
+        src: "/gallery/n8dawg42.jpg",
+        alt: "N8 EVO - Engine Bay",
+        span: "col-span-3 aspect-square max-sm:col-span-6",
+    },
+    //YellowEvo row
+    {
+        id: 18,
+        src: "/gallery/evo1.jpg",
+        alt: "Yellow EVO - Front View",
+        span: "col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 ",
+    },
+    {
+        id: 19,
+        src: "/gallery/evo4.jpg",
+        alt: "Yellow EVO - Side View",
+        span: "col-span-4 lg:col-span-4 aspect-square max-sm:col-span-12",
+    },
+    {
+        id: 20,
+        src: "/gallery/evo5.jpg",
+        alt: "Yellow EVO - Interior",
+        span: "col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 max-sm:row-start-16",
+    },
+    //EK
+    {
+        id: 21,
+        src: "/gallery/civicEK.jpg",
+        alt: "Civic EK - Front View",
+        span: "col-span-4 lg:col-span-12 aspect-[16/10] max-sm:col-span-12 max-md:col-span-12",
+    },
+    {
+        id: 22,
+        src: "/gallery/ek5.jpg",
+        alt: "Civic EK - Side View",
+        span: "col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6",
+    },
+    {
+        id: 23,
+        src: "/gallery/ek4.jpg",
+        alt: "Civic EK - Interior",
+        span: "col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6",
+    },
+    {
+        id: 24,
+        src: "/gallery/ekBay2.jpg",
+        alt: "Civic EK - Engine Bay 1",
+        span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+    },
+    {
+        id: 25,
+        src: "/gallery/ekBay1.jpg",
+        alt: "Civic EK - Engine Bay 2",
+        span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+    },
+    {
+        id: 26,
+        src: "/gallery/ek2.jpg",
+        alt: "Civic EK - Detail Shot 1",
+        span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+    },
+    {
+        id: 27,
+        src: "/gallery/ek3.jpg",
+        alt: "Civic EK - Detail Shot 2",
+        span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+    },
+    //chaser
+    {
+        id: 28,
+        src: "/gallery/chaser3.jpg",
+        alt: "Chaser - Front View",
+        span: "col-span-4 lg:col-span-8 lg:row-span-2 aspect-[4/3] max-sm:col-span-12 max-md:col-span-8",
+    },
+    {
+        id: 29,
+        src: "/gallery/chaser5.jpg",
+        alt: "Chaser - Side View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6 max-md:col-span-6",
+    },
+    {
+        id: 30,
+        src: "/gallery/chaser4.jpg",
+        alt: "Chaser - Interior",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6 max-md:col-span-6",
+    },
+    //86
+    {
+        id: 33,
+        src: "/gallery/861.jpg",
+        alt: "86 - Front View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12",
+    },
+    {
+        id: 34,
+        src: "/gallery/862.jpg",
+        alt: "86 - Side View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 35,
+        src: "/gallery/863.jpg",
+        alt: "86 - Interior",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 36,
+        src: "/gallery/864.jpg",
+        alt: "86 - Detail Shot 1",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 37,
+        src: "/gallery/865.jpg",
+        alt: "86 - Detail Shot 2",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    //jazz
+    {
+        id: 38,
+        src: "/gallery/jazz1.jpg",
+        alt: "Jazz - Front View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 39,
+        src: "/gallery/jazz2.jpg",
+        alt: "Jazz - Side View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 40,
+        src: "/gallery/jazz3.jpg",
+        alt: "Jazz - Interior",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 41,
+        src: "/gallery/jazz4.jpg",
+        alt: "Jazz - Detail Shot",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    //rari
+    {
+        id: 43,
+        src: "/gallery/rari2.jpg",
+        alt: "Ferrari - Side View",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 44,
+        src: "/gallery/rari3.jpg",
+        alt: "Ferrari - Interior",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    },
+    {
+        id: 45,
+        src: "/gallery/rari4.jpg",
+        alt: "Ferrari - Detail Shot",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12 ",
+    },
+];
