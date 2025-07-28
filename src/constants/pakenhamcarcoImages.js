69698d6b890d45133a50ca1b98012fe409ca8976
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-07-28 14:45:39 +1000

    Refactor CommercialPage to remove deprecated image constants and integrate new image sources for Acostar Logistics and Day Ones Studio.

---
+++ src/constants/pakenhamcarcoImages.js
@@ -0,0 +1,44 @@
+export const pakenhamcarcoImages = [
+    {
+        id: 1,
+        src: "/commercialGallery/PakenhamCarco/DSC02121.webp",
+        alt: "Commercial Image 1",
+        span: "col-span-8 lg:col-span-8 aspect-[16/9] max-sm:col-span-12",
+    },
+    {
+        id: 2,
+        src: "/commercialGallery/PakenhamCarco/DSC02157.webp",
+        alt: "Commercial Image 2",
+        span: "col-span-6 lg:col-span-6",
+    },
+    {
+        id: 3,
+        src: "/commercialGallery/PakenhamCarco/DSC02127.webp",
+        alt: "Commercial Image 3",
+        span: "col-span-6 lg:col-span-6 row-span-2",
+    },
+    {
+        id: 4,
+        src: "/commercialGallery/PakenhamCarco/DSC02142.webp",
+        alt: "Commercial Image 4",
+        span: "col-span-6 lg:col-span-6",
+    },
+    {
+        id: 5,
+        src: "/commercialGallery/PakenhamCarco/DSC02123.webp",
+        alt: "Commercial Image 5",
+        span: "col-span-6 lg:col-span-4",
+    },
+    {
+        id: 6,
+        src: "/commercialGallery/PakenhamCarco/DSC02134.webp",
+        alt: "Commercial Image 6",
+        span: "col-span-6 lg:col-span-4",
+    },
+    {
+        id: 7,
+        src: "/commercialGallery/PakenhamCarco/DSC02102.webp",
+        alt: "Commercial Image 7",
+        span: "col-span-6 lg:col-span-4",
+    },
+];
--- src/pages/CommercialPage.jsx
+++ src/pages/CommercialPage.jsx
@@ -1,50 +1,64 @@
-import { commercialImages } from '../constants/commercialImages';
-import GalleryImage from '../components/GalleryImage';
-import { commercialImages2 } from '../constants/commercialImages2';
+import { acostarImages } from "../constants/acostarImages";
+import GalleryImage from "../components/GalleryImage";
+import { dayonestudioImages } from "../constants/dayonestudioImages";
+import { pakenhamcarcoImages } from "../constants/pakenhamcarcoImages";
 
 const CommercialPage = () => {
-  return (
-    <>
-      
-      <section className="min-h-screen bg-black py-24">
-        <div className="container mx-auto px-8">
-              <h1 className="text-white text-heading-1 font-black text-xl xl:text-6xl lg:text-4xl md:text-3xl mt-10 mb-16">
-                Commercial Projects
-              </h1>
-          <div className="grid grid-cols-12 gap-4 lg:gap-8 grid-flow-dense my-10 mb-20 max-sm:my-6">
-            <div className="col-span-4 max-sm:col-span-12">
-              <h2 className="text-white text-heading-2 font-black text-xl xl:text-5xl lg:text-4xl md:text-3xl">
-                Acostar Logistics
-              </h2>
-              {/* some commerical work flavour text */}
-              <p></p>
-            </div>
-            {commercialImages.map((image) => (
-              <GalleryImage 
-                key={image.id}
-                {...image}
-                images={commercialImages}
-              />
-            ))}
-          </div>
-          <div className="grid grid-cols-12 gap-4 lg:gap-8 grid-flow-dense my-10">
-            <div className="col-span-4 max-sm:col-span-12">
-              <h2 className="text-white text-heading-2 font-black text-xl xl:text-5xl lg:text-4xl md:text-3xl">
-                Day Ones Studio
-              </h2>
-            </div>
-            {commercialImages2.map((image) => (
-                <GalleryImage 
-                  key={image.id}
-                  {...image}
-                  images={commercialImages2}
-                />
-              ))}
-          </div>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="min-h-screen bg-black py-24">
+                <div className="container mx-auto px-8">
+                    <h1 className="text-white text-heading-1 font-black text-xl xl:text-6xl lg:text-4xl md:text-3xl mt-10 mb-16">
+                        Commercial Projects
+                    </h1>
+                    <div className="grid grid-cols-12 gap-4 lg:gap-8 grid-flow-dense my-10">
+                        <div className="col-span-4 max-sm:col-span-12">
+                            <h2 className="text-white text-heading-2 font-black text-xl xl:text-5xl lg:text-4xl md:text-3xl">
+                                Day Ones Studio
+                            </h2>
+                        </div>
+                        {pakenhamcarcoImages.map((image) => (
+                            <GalleryImage
+                                key={image.id}
+                                {...image}
+                                images={pakenhamcarcoImages}
+                            />
+                        ))}
+                    </div>
+                    <div className="grid grid-cols-12 gap-4 lg:gap-8 grid-flow-dense my-10 mb-20 max-sm:my-6">
+                        <div className="col-span-4 max-sm:col-span-12">
+                            <h2 className="text-white text-heading-2 font-black text-xl xl:text-5xl lg:text-4xl md:text-3xl">
+                                Acostar Logistics
+                            </h2>
+                            {/* some commerical work flavour text */}
+                            <p></p>
+                        </div>
+                        {acostarImages.map((image) => (
+                            <GalleryImage
+                                key={image.id}
+                                {...image}
+                                images={acostarImages}
+                            />
+                        ))}
+                    </div>
+                    <div className="grid grid-cols-12 gap-4 lg:gap-8 grid-flow-dense my-10">
+                        <div className="col-span-4 max-sm:col-span-12">
+                            <h2 className="text-white text-heading-2 font-black text-xl xl:text-5xl lg:text-4xl md:text-3xl">
+                                Day Ones Studio
+                            </h2>
+                        </div>
+                        {dayonestudioImages.map((image) => (
+                            <GalleryImage
+                                key={image.id}
+                                {...image}
+                                images={dayonestudioImages}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default CommercialPage;
