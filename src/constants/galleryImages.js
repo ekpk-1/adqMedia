98d736578ab1098cb0ff5df75536b237d3cdc895
ekp-1 <138497015+ekpk-1@users.noreply.github.com>
2025-12-04 17:11:59 +1100

    Add new gallery images for S2K, M2, R32, and others; refactor ShowcasePage for improved tab functionality and image handling.

--- src/constants/galleryImages.js
+++ src/constants/galleryImages.js
@@ -1,307 +1,427 @@
+// // Common span configurations for reusability
+const SPANS = {
+    FULL: "col-span-12",
+    HALF: "col-span-6",
+    THIRD: "col-span-4",
+    QUARTER: "col-span-3",
+    FULL_WIDE:
+        "col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]",
+    HALF_SQUARE:
+        "col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]",
+    THIRD_SQUARE: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+    QUARTER_SQUARE: "col-span-3 aspect-square max-sm:col-span-6",
+};
+
 export const galleryImages = [
-    //Yellow996
+    // S2K - Japan
     {
-        id: 46,
+        src: "/gallery/s2k/1.webp",
+        alt: "S2K - Front View",
+        span: SPANS.FULL,
+        group: "s2k",
+    },
+    {
+        src: "/gallery/s2k/2.webp",
+        alt: "S2K - Side View",
+        span: SPANS.FULL,
+        group: "s2k",
+    },
+    {
+        src: "/gallery/s2k/3.webp",
+        alt: "S2K - Side View",
+        span: SPANS.QUARTER,
+        group: "s2k",
+    },
+    {
+        src: "/gallery/s2k/4.webp",
+        alt: "S2K - Side View",
+        span: SPANS.QUARTER,
+        group: "s2k",
+    },
+    {
+        src: "/gallery/s2k/5.webp",
+        alt: "S2K - Side View",
+        span: SPANS.QUARTER,
+        group: "s2k",
+    },
+    {
+        src: "/gallery/s2k/6.webp",
+        alt: "S2K - Side View",
+        span: SPANS.QUARTER,
+        group: "s2k",
+    },
+    // M2 4/12/2025
+    {
+        src: "/gallery/m2/1.webp",
+        alt: "M2 - Front View",
+        span: SPANS.FULL,
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/2.webp",
+        alt: "M2 - Side View",
+        span: SPANS.FULL,
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/3.webp",
+        alt: "M2 - Side View",
+        span: SPANS.THIRD,
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/4.webp",
+        alt: "M2 - Side View",
+        span: SPANS.THIRD,
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/5.webp",
+        alt: "M2 - Front View",
+        span: SPANS.THIRD,
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/6.webp",
+        alt: "M2 - Side View",
+        span: "col-span-6 row-span-2 aspect-[3/4]",
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/7.webp",
+        alt: "M2 - Side View",
+        span: "col-span-6 row-span-1 aspect-[16/10]",
+        group: "m2",
+    },
+    {
+        src: "/gallery/m2/8.webp",
+        alt: "M2 - Side View",
+        span: "col-span-6 row-span-1 aspect-[16/10]",
+        group: "m2",
+    },
+    // R32 4/12/2025
+    {
+        src: "/gallery/r32/1.webp",
+        alt: "Black R32 - Side View",
+        span: SPANS.FULL,
+        group: "r32x2",
+    },
+    {
+        src: "/gallery/r32/3.webp",
+        alt: "Black R32 - Side View",
+        span: SPANS.THIRD,
+        group: "r32x2",
+    },
+    {
+        src: "/gallery/r32/5.webp",
+        alt: "Black R32 - Side View",
+        span: SPANS.THIRD,
+        group: "r32x2",
+    },
+    {
+        src: "/gallery/r32/4.webp",
+        alt: "Black R32 - Side View",
+        span: SPANS.THIRD,
+        group: "r32x2",
+    },
+    {
+        src: "/gallery/r32/2.webp",
+        alt: "Black R32 - Side View",
+        span: SPANS.FULL,
+        group: "r32x2",
+    },
+
+    // Yellow 996 Porsche
+    {
         src: "/gallery/996-1.webp",
-        alt: "",
+        alt: "Yellow 996 - Front View",
         span: "col-span-12",
+        group: "996",
     },
     {
-        id: 47,
         src: "/gallery/996-2.webp",
-        alt: "",
+        alt: "Yellow 996 - Side View",
         span: "col-span-4",
+        group: "996",
     },
     {
-        id: 48,
         src: "/gallery/996-3.webp",
-        alt: "",
+        alt: "Yellow 996 - Detail Shot 1",
         span: "col-span-4",
+        group: "996",
     },
     {
-        id: 49,
         src: "/gallery/996-4.webp",
-        alt: "",
+        alt: "Yellow 996 - Detail Shot 2",
         span: "col-span-4",
+        group: "996",
     },
     {
-        id: 50,
         src: "/gallery/996-5.webp",
-        alt: "",
+        alt: "Yellow 996 - Interior",
         span: "col-span-6",
+        group: "996",
     },
     {
-        id: 51,
         src: "/gallery/996-6.webp",
-        alt: "",
+        alt: "Yellow 996 - Engine Bay",
         span: "col-span-6",
+        group: "996",
     },
     {
-        id: 52,
         src: "/gallery/996-7.webp",
-        alt: "",
+        alt: "Yellow 996 - Rear View",
         span: "col-span-12",
+        group: "996",
     },
-    //rushsM3
+
+    // Rush's M3
     {
-        id: 1,
         src: "/gallery/rushsM3-1.jpg",
         alt: "Rushs M3 - Front View",
         span: "col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]",
+        group: "rushsM3",
     },
     {
-        id: 2,
         src: "/gallery/rushsM3-5.jpg",
         alt: "Rushs M3 - Side View",
         span: "col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]",
+        group: "rushsM3",
     },
     {
-        id: 3,
         src: "/gallery/rushsM3-3.jpg",
         alt: "Rushs M3 - Interior",
         span: "col-span-6 lg:col-span-6 aspect-[4/3] max-sm:col-span-6 max-sm:aspect-[4/3]",
+        group: "rushsM3",
     },
     {
-        id: 4,
         src: "/gallery/rushsM3-4.jpg",
         alt: "Rushs M3 - Detail Shot",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "rushsM3",
     },
     {
-        id: 5,
         src: "/gallery/rushsM3-6.jpg",
         alt: "Rushs M3 - Rear View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "rushsM3",
     },
     {
-        id: 6,
         src: "/gallery/rushsM3-2.jpg",
         alt: "Rushs M3 - Profile View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12",
+        group: "rushsM3",
     },
-    //tookieEVO
+
+    // Tookie's EVO
     {
-        id: 7,
         src: "/gallery/tookie3.jpg",
         alt: "Tookie EVO - Front View",
         span: "col-span-12 lg:col-span-12 aspect-[16/9] max-sm:col-span-12 max-sm:aspect-[4/3]",
+        group: "tookieEVO",
     },
     {
-        id: 8,
         src: "/gallery/tookie1.jpg",
         alt: "Tookie EVO - Side View",
         span: "col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]",
+        group: "tookieEVO",
     },
     {
-        id: 9,
         src: "/gallery/tookie2.jpg",
         alt: "Tookie EVO - Interior",
         span: "col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-6 max-sm:aspect-[4/3]",
+        group: "tookieEVO",
     },
     {
-        id: 10,
         src: "/gallery/tookie5.jpg",
         alt: "Tookie EVO - Detail Shot",
         span: "col-span-4 lg:col-span-4 aspect-[3/4] max-sm:col-span-12 max-sm:aspect-[2/1]",
+        group: "tookieEVO",
     },
     {
-        id: 11,
         src: "/gallery/tookie4.jpg",
         alt: "Tookie EVO - Engine Bay",
         span: "col-span-4 lg:col-span-8 aspect-[2/1] max-sm:col-span-6 max-sm:aspect-[3/3] max-md:col-span-6",
+        group: "tookieEVO",
     },
     {
-        id: 12,
         src: "/gallery/tookie6.jpg",
         alt: "Tookie EVO - Rear View",
         span: "col-span-4 lg:col-span-4 aspect-[4/4.2] max-sm:col-span-6 max-sm:aspect-[3/3] max-md:aspect-[2/1] max-md:col-span-6",
+        group: "tookieEVO",
     },
 
-    //N8Evo row
+    // N8's EVO
     {
-        id: 13,
         src: "/gallery/n8dawg3.jpg",
         alt: "N8 EVO - Front View",
         span: "col-span-6 aspect-square row-span-2 max-sm:col-span-12 max-sm:row-span-1",
+        group: "n8EVO",
     },
     {
-        id: 14,
         src: "/gallery/n8dawg43.jpg",
         alt: "N8 EVO - Side View",
         span: "col-span-3 aspect-square max-sm:col-span-6",
+        group: "n8EVO",
     },
     {
-        id: 15,
         src: "/gallery/n8dawg45.jpg",
         alt: "N8 EVO - Interior",
         span: "col-span-3 aspect-square max-sm:col-span-6",
+        group: "n8EVO",
     },
     {
-        id: 16,
         src: "/gallery/evoN83.jpg",
         alt: "N8 EVO - Detail Shot",
         span: "col-span-3 aspect-square max-sm:col-span-6",
+        group: "n8EVO",
     },
     {
-        id: 17,
         src: "/gallery/n8dawg42.jpg",
         alt: "N8 EVO - Engine Bay",
         span: "col-span-3 aspect-square max-sm:col-span-6",
+        group: "n8EVO",
     },
-    //YellowEvo row
+
+    // Civic EK
     {
-        id: 18,
-        src: "/gallery/evo1.jpg",
-        alt: "Yellow EVO - Front View",
-        span: "col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 ",
-    },
-    {
-        id: 19,
-        src: "/gallery/evo4.jpg",
-        alt: "Yellow EVO - Side View",
-        span: "col-span-4 lg:col-span-4 aspect-square max-sm:col-span-12",
-    },
-    {
-        id: 20,
-        src: "/gallery/evo5.jpg",
-        alt: "Yellow EVO - Interior",
-        span: "col-span-4 lg:col-span-4 aspect-square max-sm:col-span-6 max-sm:row-start-16",
-    },
-    //EK
-    {
-        id: 21,
         src: "/gallery/civicEK.jpg",
         alt: "Civic EK - Front View",
         span: "col-span-4 lg:col-span-12 aspect-[16/10] max-sm:col-span-12 max-md:col-span-12",
+        group: "civicEK",
     },
     {
-        id: 22,
         src: "/gallery/ek5.jpg",
         alt: "Civic EK - Side View",
         span: "col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6",
+        group: "civicEK",
     },
     {
-        id: 23,
         src: "/gallery/ek4.jpg",
         alt: "Civic EK - Interior",
         span: "col-span-4 lg:col-span-6 aspect-[4/3] max-sm:col-span-12 max-md:col-span-6",
+        group: "civicEK",
     },
     {
-        id: 24,
         src: "/gallery/ekBay2.jpg",
         alt: "Civic EK - Engine Bay 1",
         span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+        group: "civicEK",
     },
     {
-        id: 25,
         src: "/gallery/ekBay1.jpg",
         alt: "Civic EK - Engine Bay 2",
         span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+        group: "civicEK",
     },
     {
-        id: 26,
         src: "/gallery/ek2.jpg",
         alt: "Civic EK - Detail Shot 1",
         span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+        group: "civicEK",
     },
     {
-        id: 27,
         src: "/gallery/ek3.jpg",
         alt: "Civic EK - Detail Shot 2",
         span: "col-span-4 lg:col-span-3 aspect-[3/4] max-sm:col-span-6",
+        group: "civicEK",
     },
-    //chaser
+
+    // Chaser
     {
-        id: 28,
         src: "/gallery/chaser3.jpg",
         alt: "Chaser - Front View",
         span: "col-span-4 lg:col-span-8 lg:row-span-2 aspect-[4/3] max-sm:col-span-12 max-md:col-span-8",
+        group: "chaser",
     },
     {
-        id: 29,
         src: "/gallery/chaser5.jpg",
         alt: "Chaser - Side View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6 max-md:col-span-6",
+        group: "chaser",
     },
     {
-        id: 30,
         src: "/gallery/chaser4.jpg",
         alt: "Chaser - Interior",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6 max-md:col-span-6",
+        group: "chaser",
     },
-    //86
+
+    // Toyota 86
     {
-        id: 33,
         src: "/gallery/861.jpg",
         alt: "86 - Front View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12",
+        group: "86",
     },
     {
-        id: 34,
         src: "/gallery/862.jpg",
         alt: "86 - Side View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "86",
     },
     {
-        id: 35,
         src: "/gallery/863.jpg",
         alt: "86 - Interior",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "86",
     },
     {
-        id: 36,
         src: "/gallery/864.jpg",
         alt: "86 - Detail Shot 1",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "86",
     },
     {
-        id: 37,
         src: "/gallery/865.jpg",
         alt: "86 - Detail Shot 2",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "86",
     },
-    //jazz
+
+    // Honda Jazz
     {
-        id: 38,
         src: "/gallery/jazz1.jpg",
         alt: "Jazz - Front View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "jazz",
     },
     {
-        id: 39,
         src: "/gallery/jazz2.jpg",
         alt: "Jazz - Side View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "jazz",
     },
     {
-        id: 40,
         src: "/gallery/jazz3.jpg",
         alt: "Jazz - Interior",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "jazz",
     },
     {
-        id: 41,
         src: "/gallery/jazz4.jpg",
         alt: "Jazz - Detail Shot",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "jazz",
     },
-    //rari
+
+    // Ferrari
     {
-        id: 43,
         src: "/gallery/rari2.jpg",
         alt: "Ferrari - Side View",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "ferrari",
     },
     {
-        id: 44,
         src: "/gallery/rari3.jpg",
         alt: "Ferrari - Interior",
         span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-6",
+        group: "ferrari",
     },
     {
-        id: 45,
         src: "/gallery/rari4.jpg",
         alt: "Ferrari - Detail Shot",
-        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12 ",
+        span: "col-span-4 lg:col-span-4 aspect-[4/3] max-sm:col-span-12",
+        group: "ferrari",
     },
 ];
--- src/pages/ShowcasePage.jsx
+++ src/pages/ShowcasePage.jsx
@@ -1,81 +1,83 @@
-import { useState } from 'react';
-import video from '../assets/0006bgvid.mp4'
-import { galleryImages } from '../constants/galleryImages';
-import { galleryVideos } from '../constants/galleryVideos';
-import GalleryImage from '../components/GalleryImage';
-import GalleryVideo from '../components/GalleryVideo';
+import { useState } from "react";
+import video from "../assets/0006bgvid.mp4";
+import { galleryImages } from "../constants/galleryImages";
+import { galleryVideos } from "../constants/galleryVideos";
+import GalleryImage from "../components/GalleryImage";
+import GalleryVideo from "../components/GalleryVideo";
 
 const ShowcasePage = () => {
-  const [activeTab, setActiveTab] = useState('photos');
+    const [activeTab, setActiveTab] = useState("photos");
 
-  return (
-    <>
-      <div className="h-screen text-white flex justify-center items-center text-heading-1 font-black">
-        <video 
-          src={video} 
-          autoPlay 
-          muted 
-          loop 
-          playsInline
-          disablePictureInPicture
-          className="absolute inset-0 object-cover w-full h-full z-0 pointer-events-none" 
-          style={{ 
-            WebkitUserSelect: 'none',
-            msUserSelect: 'none',
-            userSelect: 'none',
-          }}
-        />
-        <div className="absolute inset-0 bg-black/50 z-[1]"></div>
-      </div>
-      <section className="min-h-screen bg-black py-12">
-        <div className="container mx-auto px-8">
-          <div className="flex justify-center mb-12">
-            <div className="inline-flex rounded-lg bg-white/10 p-1">
-              <button
-                onClick={() => setActiveTab('photos')}
-                className={`px-6 py-2 rounded-lg transition-all duration-300 ${
-                  activeTab === 'photos'
-                    ? 'bg-white text-black'
-                    : 'text-white hover:bg-white/10'
-                }`}
-              >
-                Photos
-              </button>
-              <button
-                onClick={() => setActiveTab('videos')}
-                className={`px-6 py-2 rounded-lg transition-all duration-300 ${
-                  activeTab === 'videos'
-                    ? 'bg-white text-black'
-                    : 'text-white hover:bg-white/10'
-                }`}
-              >
-                Videos
-              </button>
+    return (
+        <>
+            <div className="h-screen text-white flex justify-center items-center text-heading-1 font-black">
+                <video
+                    src={video}
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    disablePictureInPicture
+                    className="absolute inset-0 object-cover w-full h-full z-0 pointer-events-none"
+                    style={{
+                        WebkitUserSelect: "none",
+                        msUserSelect: "none",
+                        userSelect: "none",
+                    }}
+                />
+                <div className="absolute inset-0 bg-black/50 z-[1]"></div>
             </div>
-          </div>
-          <div className="grid grid-cols-12 gap-8 max-sm:gap-4">
-            {activeTab === 'photos' ? (
-              galleryImages.map((image) => (
-                <GalleryImage 
-                  key={image.id}
-                  {...image}
-                  images={galleryImages}
-                />
-              ))
-            ) : (
-              galleryVideos.map((video) => (
-                <GalleryVideo
-                  key={video.id}
-                  {...video}
-                  videos={galleryVideos}
-                />
-              ))
-            )}
-          </div>
-        </div>
-      </section>
-    </>
-  )
-}
+            <section className="min-h-screen bg-black py-12">
+                <div className="container mx-auto w-full  px-8">
+                    <div className="flex justify-center mb-12 w-full">
+                        <div className="inline-flex rounded-lg bg-white/10 p-1">
+                            <button
+                                onClick={() => setActiveTab("photos")}
+                                className={`px-6 py-2 rounded-lg transition-all duration-300 ${
+                                    activeTab === "photos"
+                                        ? "bg-white text-black"
+                                        : "text-white hover:bg-white/10"
+                                }`}
+                            >
+                                Photos
+                            </button>
+                            <button
+                                onClick={() => setActiveTab("videos")}
+                                className={`px-6 py-2 rounded-lg transition-all duration-300 ${
+                                    activeTab === "videos"
+                                        ? "bg-white text-black"
+                                        : "text-white hover:bg-white/10"
+                                }`}
+                            >
+                                Videos
+                            </button>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-12 gap-8 max-sm:gap-4 w-full">
+                        {activeTab === "photos"
+                            ? galleryImages.map((image, index) => (
+                                  <GalleryImage
+                                      key={index}
+                                      {...image}
+                                      id={index}
+                                      images={galleryImages.map((img, idx) => ({
+                                          ...img,
+                                          id: idx,
+                                      }))}
+                                  />
+                              ))
+                            : galleryVideos.map((video) => (
+                                  <GalleryVideo
+                                      key={video.id}
+                                      {...video}
+                                      videos={galleryVideos}
+                                  />
+                              ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
 
-export default ShowcasePage
+export default ShowcasePage;
